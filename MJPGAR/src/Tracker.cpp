55ce9c879627de498866fc2a47c40f6522f09614
Siharat Thammaya <siharat.th@gmail.com>
2016-08-06 22:58:20 +0700

    .

---
+++ MJPGAR/src/Tracker.cpp
@@ -0,0 +1,9 @@
+//
+//  Tracker.cpp
+//  MJPGAR
+//
+//  Created by SIHARAT THAMMAYA on 8/6/2559 BE.
+//
+//
+
+#include "Tracker.h"
---
+++ MJPGAR/src/Tracker.h
@@ -0,0 +1,18 @@
+//
+//  Tracker.hpp
+//  MJPGAR
+//
+//  Created by SIHARAT THAMMAYA on 8/6/2559 BE.
+//
+//
+
+#ifndef Tracker_hpp
+#define Tracker_hpp
+
+#include "ofxARTPattern.h"
+
+class Tracker : public ofxArtool5::PatternTracker{
+    
+};
+
+#endif /* Tracker_hpp */
